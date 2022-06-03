7547a7739091af6811d2368b81e3bef975bb474e
Vrynna <Julian.Kuschnerus@outlook.de>
2022-06-04 00:13:13 +0200

    useState added, counter implement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Todolist from "./components/Todolist";
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
   return (
 
       <div className="max-w-xl m-auto bg-gray-200 mt-20 ">
