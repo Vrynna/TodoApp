def1448dbb0df0dd0603c79544d2fa5e0f8dc711
Vrynna <Julian.Kuschnerus@outlook.de>
2022-07-09 23:44:37 +0200

    Format

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from "react";
 
-const Todo = ({ description, done, onChangeTodo,  onDeleteTodo, index }) => {
-
+const Todo = ({ description, done, onChangeTodo, onDeleteTodo, index }) => {
   return (
     <div>
-        <div className={done ? "flex justify-between item-center p-2 bg-green-600 text-white" : "flex justify-between item-center p-2 bg-red-500 text-white"}>
+      <div
+        className={
+          done
+            ? "flex justify-between item-center p-2 bg-green-600 text-white"
+            : "flex justify-between item-center p-2 bg-red-500 text-white"
+        }
+      >
+        <h1
+          className="text-lg cursor-pointer"
+          onClick={() => {
+            onChangeTodo(index);
+          }}
+        >
+          {description}
+        </h1>
 
-            <h1 className="text-lg cursor-pointer" onClick={() => {onChangeTodo(index)}}>{description}</h1>
-
-            <button className="text-lg bg-gray-400 p-2 text-white" onClick={() => {onDeleteTodo(index)}}>Löschen</button>
-        </div>    
+        <button
+          className="text-lg bg-gray-400 p-2 text-white"
+          onClick={() => {
+            onDeleteTodo(index);
+          }}
+        >
+          Löschen
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import Todo from './Todo'
+import React, { useEffect, useState } from "react";
+import Todo from "./Todo";
 
 // const alltodos = [
 //     {description : "Einkaufen", done : true},
@@ -8,80 +8,94 @@ import Todo from './Todo'
 // ];
 
 const Todolist = () => {
+  //states zum speichern und ändern von Daten
+  const [openCount, countOpenTodos] = useState(0);
+  const [todos, setTodos] = useState(
+    /*alltodos*/ /*[]*/ () => {
+      const items = localStorage.getItem("items");
+      const parsed = JSON.parse(items);
+      return parsed;
+    },
+  );
+  const [textinput, settextinput] = useState("");
 
-    //states zum speichern und ändern von Daten
-    const [openCount, countOpenTodos] = useState(0);
-    const [todos, setTodos] = useState(/*alltodos*/ /*[]*/() => {
-        const items = localStorage.getItem("items");
-        const parsed = JSON.parse(items);
-        return parsed;
-    });
-    const [textinput, settextinput] = useState("");
+  //Event-handler function die den Text erfasst.
+  const changeText = (e) => {
+    settextinput(e.target.value);
+  };
 
+  const submit = (e) => {
+    e.preventDefault();
 
-    //Event-handler function die den Text erfasst.
-    const changeText = (e) => {
-        settextinput(e.target.value);
-    }
+    const newTodos = [...todos, { description: textinput, done: false }];
+    setTodos(newTodos);
+  };
 
-    const submit = (e) => {
-        e.preventDefault();
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
-        const newTodos = [...todos, { description: textinput, done: false }];
-        setTodos(newTodos);
-    }
-
-
-
-    const deleteTodo = (index) => {
-        const newTodos = [...todos];
-        newTodos.splice(index, 1);
-        setTodos(newTodos);
-    }
-
-    useEffect(() => {
-
-        const countOpen = () => {
-            const donetodos = todos.filter((item) => {
-                return !item.done
-            });
-            countOpenTodos(donetodos.length);
-        };
-
-        localStorage.setItem("items", JSON.stringify(todos));
-
-        countOpen();
-    }, [todos]);
-
-
-
-    const changeTodo = (index) => {
-        const newTodos = [...todos];
-        if (newTodos[index].done) {
-            newTodos[index].done = false;
-        } else {
-            newTodos[index].done = true;
-        }
-        setTodos(newTodos);
+  useEffect(() => {
+    const countOpen = () => {
+      const donetodos = todos.filter((item) => {
+        return !item.done;
+      });
+      countOpenTodos(donetodos.length);
     };
 
-    return (
-        <div className="shadow-sm hover:shadow-lg">
-            <div className="text-center bg-gray-900 text-white py-4 font-semibold">
-                <h1 className="text-3xl">Meine Todos</h1>
-                <h2>Offene Todos: {openCount}</h2>
-                <form className="grid grid-cols-3 py-2">
-                    <input type="text" placeholder="Neues Todo..." className="col-span-2 py-2 text-gray-900" onChange={changeText}></input>
-                    <input type="submit" value="Add Todo" className="col-span-1 cursor-pointer" onClick={submit}></input>
-                </form>
-            </div>
-            {todos.map((item, index) => {
-                return (
-                    <Todo description={item.description} done={item.done} key={index} index={index} onChangeTodo={changeTodo} onDeleteTodo={deleteTodo}></Todo>
-                );
-            })}
-        </div>
-    );
-}
+    localStorage.setItem("items", JSON.stringify(todos));
+
+    countOpen();
+  }, [todos]);
+
+  const changeTodo = (index) => {
+    const newTodos = [...todos];
+    if (newTodos[index].done) {
+      newTodos[index].done = false;
+    } else {
+      newTodos[index].done = true;
+    }
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="shadow-sm hover:shadow-lg">
+      <div className="text-center bg-gray-900 text-white py-4 font-semibold">
+        <h1 className="text-3xl">Meine Todos</h1>
+        <h2>Offene Todos: {openCount}</h2>
+        <form className="grid grid-cols-3 py-2">
+          <input
+            type="text"
+            placeholder="Neues Todo..."
+            className="col-span-2 py-2 text-gray-900"
+            onChange={changeText}
+          ></input>
+
+          <input
+            type="submit"
+            value="Add Todo"
+            className="col-span-1 cursor-pointer"
+            onClick={submit}
+          ></input>
+        </form>
+      </div>
+
+      {todos.map((item, index) => {
+        return (
+          <Todo
+            description={item.description}
+            done={item.done}
+            key={index}
+            index={index}
+            onChangeTodo={changeTodo}
+            onDeleteTodo={deleteTodo}
+          ></Todo>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Todolist;
